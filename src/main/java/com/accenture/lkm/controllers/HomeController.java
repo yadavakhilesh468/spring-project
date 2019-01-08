2dd3a955c1cfbfdf879638d89ac68825be318a83
Akhilesh Yadav <akhilesh.a.yadav@accenture.com>
2019-01-08 10:34:00 +0530

    pipeline

--- src/main/java/com/accenture/lkm/controllers/HomeController.java
+++ src/main/java/com/accenture/lkm/controllers/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("title", "Accenture - High performance. Delivered");
-		mv.addObject("greeting", "Jenkins Pipelines");
+		mv.addObject("greeting", "Jenkin Pipelines");
 		return mv;
 	}
 	
